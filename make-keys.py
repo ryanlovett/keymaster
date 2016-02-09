a9b570679ce660ec8dccc0d8f683821fe4c0d05b
ryanlovett <rylo@berkeley.edu>
2016-02-09 18:13:18 +0000

    Add option to run for specific hosts.

--- make-keys.py
+++ make-keys.py
@@ -5,11 +5,20 @@
 # 
 # Runs from jupyterhub-deploy/ and uses "inventory" and "vault-password" files.
 
+import sys
 import os
 import subprocess
 import ansible.inventory
+import argparse
 
 DEBUG = False
+
+parser = argparse.ArgumentParser()
+specific_hosts = False
+if len(sys.argv) > 1:
+	specific_hosts = True
+	args, arg_hosts = parser.parse_known_args(sys.argv[1:])
+
 
 KEYMASTER = ["docker", "run", "--rm", "-v",
 	os.getcwd() + "/../certificates/:/certificates/", "ds/keymaster"]
@@ -22,6 +31,8 @@ subprocess.call(KEYMASTER + ["ca"])
 vault_password = open('../jupyterhub-deploy/vault-password').read().strip()
 inv = ansible.inventory.Inventory(host_list='../jupyterhub-deploy/inventory', vault_password=vault_password)
 for host in inv.get_group('all').get_hosts():
+	if specific_hosts and host.name not in arg_hosts:
+		continue
 	keyname = host.name
 	ip = host.vars['fqdn']
 	hostname = host.vars['ansible_ssh_host']
